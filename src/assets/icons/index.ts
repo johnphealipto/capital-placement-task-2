b3555a2127ff4d29e78b0375e8e5d90ef24bebff
johnphealipto <johnadibe450@gmail.com>
2023-09-28 11:31:33 +0100

    implemented side navigation

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,17 +1,19 @@
 import CalendarIcon from "./calendar.svg?react";
-import ChevronRightIcon from "./chevron-right.svg";
-import FileIcon from "./file.svg";
-import HeartIcon from "./heart.svg";
-import NotebookIcon from "./notebook.svg";
-import SettingsIcon from "./settings.svg";
-import ShareIcon from "./share.svg";
-import UserIcon from "./user.svg";
+import ChevronRightIcon from "./chevron-right.svg?react";
+import FileIcon from "./file.svg?react";
+import HeartIcon from "./heart.svg?react";
+import HomeIcon from "./home.svg?react";
+import NotebookIcon from "./notebook.svg?react";
+import SettingsIcon from "./settings.svg?react";
+import ShareIcon from "./share.svg?react";
+import UserIcon from "./user.svg?react";
 
 export {
   CalendarIcon,
   ChevronRightIcon,
   FileIcon,
   HeartIcon,
+  HomeIcon,
   NotebookIcon,
   SettingsIcon,
   ShareIcon,
--- src/layout/side-navigation/index.tsx
+++ src/layout/side-navigation/index.tsx
@@ -1,9 +1,52 @@
+import {
+  CalendarIcon,
+  ChevronRightIcon,
+  FileIcon,
+  HeartIcon,
+  HomeIcon,
+  NotebookIcon,
+  SettingsIcon,
+  ShareIcon,
+  UserIcon,
+} from "@/assets/icons";
+import classNames from "classnames";
+
+const NAV_ICONS = [
+  <HomeIcon className="h-5 w-5" />,
+  <UserIcon className="h-5 w-5" />,
+  <CalendarIcon className="h-5 w-5" />,
+  <ShareIcon className="h-5 w-5" />,
+  <FileIcon className="h-5 w-5" />,
+  <NotebookIcon className="h-5 w-5" />,
+  <HeartIcon className="h-5 w-5" />,
+  <ChevronRightIcon className="h-4 w-4" />,
+];
+
 const SideNavigation = () => {
   return (
-    <div
-      id="side-navigation"
-      className="bg-white flex flex-col items-center justify-between py-12 shadow-[0_4px_23px_0_rgba(0,0,0,.05)]"
-    >fgsdfg</div>
+    <div id="side-navigation">
+      <div className="flex flex-col items-center gap-8">
+        <div className="bg-[#D9D9D9] w-8 h-8 rounded-full" />
+        <ul className="flex flex-col items-center gap-6">
+          {NAV_ICONS.map((item, idx) => (
+            <li
+              key={idx}
+              className={classNames("p-[15px] rounded-lg", {
+                "bg-[#E9EFFF]": idx === 3,
+              })}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="flex flex-col items-center gap-9">
+        <SettingsIcon className="h-6 w-6" />
+        <div className="bg-[#D7E5FD] w-8 h-8 flex rounded-full">
+          <span className="text-[#B1CDFD] text-sm font-bold m-auto">AS</span>
+        </div>
+      </div>
+    </div>
   );
 };
 
