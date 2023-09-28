c77cb7f5ca14070c31bf2798d55695a997f870a0
johnphealipto <johnadibe450@gmail.com>
2023-09-28 10:39:45 +0100

    extracted nav icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,19 @@
+import CalendarIcon from "./calendar.svg?react";
+import ChevronRightIcon from "./chevron-right.svg";
+import FileIcon from "./file.svg";
+import HeartIcon from "./heart.svg";
+import NotebookIcon from "./notebook.svg";
+import SettingsIcon from "./settings.svg";
+import ShareIcon from "./share.svg";
+import UserIcon from "./user.svg";
+
+export {
+  CalendarIcon,
+  ChevronRightIcon,
+  FileIcon,
+  HeartIcon,
+  NotebookIcon,
+  SettingsIcon,
+  ShareIcon,
+  UserIcon,
+};
